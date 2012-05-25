6d959369f60b85e6b23197ae277f5a76ef4947f1
Thijs van Dijk <github@inurbanus.nl>
2012-05-25 11:23:47 +0200

    Removed the empty tab.

--- view/tabs/object-browser.js
+++ view/tabs/object-browser.js
@@ -2,17 +2,17 @@
 	Copyright (C) 2012 Thijs van Dijk
 	
 	This file is part of Superprofiler.
-
+	
 	Superprofiler is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
-
+	
 	Superprofiler is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU General Public License for more details.
-
+	
 	You should have received a copy of the GNU General Public License
 	along with Superprofiler.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -48,17 +48,6 @@ Profiler.tabs.push({
 			}
 		};
 		return ob_b;
-	})()
-});
-
-Profiler.tabs.push({
-	"title": "Empty tab",
-	"id":    "empty-tab",
-	"renderer": (function(){
-		return (function( inp, out ){
-			
-			return out.append( $("<h2/>").html("Yep, this tab is empty.") );
-		});
 	})()
 });
 
