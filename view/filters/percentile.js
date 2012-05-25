8cb78ecb48ae8584525d3ef809e6bb698b817b70
Thijs van Dijk <github@inurbanus.nl>
2012-05-25 11:37:27 +0200

    Added a percentile filter

--- view/filters/percentile.js
+++ view/filters/percentile.js
@@ -20,7 +20,36 @@
 // A simple percentile calculation.
 // 
 // Within each activity type, assign a percentile score to the duration of the
-// activity.
+// activity. In spite of their name, percentiles range between 0 and 1.
 
 // Requires: activity-sort
+
+
+Profiler.filters.push(function(data){
+	
+	if ( ! data.activities )
+	{
+		console.warn( "This filter requires the [activity-sort] filter." );
+	}
+	
+	var add_perc = function( acts )
+	{
+		if ( acts.length < 2 ) return;
+		
+		// Make a shallow copy of acts we can sort freely
+		var sacts = [];
+		for ( var i = 0; i < acts.length; i++ )
+			sacts.push(acts[i]);
+		
+		// Sort activities in sacts by their duration
+		sacts.sort(function(a,b){ return a.dur - b.dur; });
+		
+		var max = sacts.length - 1;
+		for ( var i = 0; i <= max; i++ )
+			sacts[i].duration_percentile = i / max;
+	};
+	
+	for ( i in data.activities )
+		add_perc( data.activities[i] );
+});
 
